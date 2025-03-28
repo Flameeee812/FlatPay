ce8ba5c488b05327f2a798a08244781f624b316d
Mikhail Kharchenko <misha2006060906@gmail.com>
2025-03-28 14:55:27 +0300

    upd server directory

--- server/logger/setup_logger.py
+++ server/logger/setup_logger.py
@@ -1,6 +1,14 @@
 import logging.config
+import os
 
 import yaml
+
+from ..config import config
+
+
+def create_log_dir():
+    if not os.path.exists(config.LOG_PATH):
+        os.makedirs(config.LOG_PATH)
 
 
 def setup_logger_func() -> None:
@@ -8,8 +16,10 @@ def setup_logger_func() -> None:
     Настройка логгера с использованием конфигурации из config.yaml файла.
     """
 
+    create_log_dir()
+
     with open(file="server/logger/config.yaml", mode="rt") as f:
-        config = yaml.safe_load(f)
-    logging.config.dictConfig(config)
+        log_config = yaml.safe_load(f)
+    logging.config.dictConfig(log_config)
 
     return None
